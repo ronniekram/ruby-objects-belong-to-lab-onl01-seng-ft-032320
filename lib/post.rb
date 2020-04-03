75df0865e326236682123b0bb9f1b6913d962e6e
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-03 18:03:06 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++
@@ -1,9 +0,0 @@
-class Post 
-  #Post class needs access to Author class -- done through attr_accessor
-  attr_accessor :title, :author 
-  
-  #do not need an argument because no additional information is required outside of title 
-  def initialize
-    @title = title
-  end 
-end 
